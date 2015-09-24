8898884909b2f81c23c610c0e6c9a2992e4f83f0
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-09-24 18:27:18 +0300

    started walking through OpenGL tutorial

---
+++ CMakeSrc.cmake
@@ -0,0 +1,19 @@
+# C++ source files
+set(SOURCE_FILES
+${SourcePath}/main.cpp
+)
+
+#set(SOURCE_FILES_GLFW
+#${ExtPath}/GLFW/glfw3.h
+#${ExtPath}/GLFW/glfw3native.h
+#)
+
+# set appropriate source groups
+#source_group(glfw  FILES  ${SOURCE_FILES_GLFW})
+
+
+set(SOURCE_FILES_ALL
+${SOURCE_FILES}
+#${SOURCE_FILES_GLFW}
+)
+
---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+size_t const sWinWidth = 640;
+size_t const sWinHeight = 480;
+
+void glfwErrorReporting(int errCode, char const* msg);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int modifiers);
+
+int main(void)
+{
+    if (GL_FALSE == glfwInit())
+        return -1;
+        // Provide some hints for the GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    glfwSetErrorCallback(glfwErrorReporting);
+
+        // Create a window
+    GLFWwindow* window = glfwCreateWindow(sWinWidth, sWinHeight, "Renderize", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+        // Initialize Glew
+    
+    glewExperimental = GL_TRUE;
+    GLenum glewCode = glewInit();
+    if (GLEW_OK != glewCode)
+    {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(glewCode) << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, sWinWidth, sWinHeight);
+
+        // Initialize some GLFW callback
+    glfwSetKeyCallback(window, keyCallback);
+
+        // Game Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+            // Rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void glfwErrorReporting(int errCode, char const* msg)
+{
+    std::cerr << "ERROR " << errCode << ": " << msg << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int modifiers)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
