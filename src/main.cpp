d8f48be13a10193e9f5efd31eab634dd45f4b0df
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-09-25 00:42:20 +0300

    creating vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,16 @@ int main(void)
         // Initialize some GLFW callback
     glfwSetKeyCallback(window, keyCallback);
 
+    GLfloat vertices[] = {
+        -0.5f, 0.5f, 0.0f,
+         0.5f, 0.5f, 0.0f,
+         0.0f, 0.0f, 0.0f
+    };
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
         // Game Loop
     while (!glfwWindowShouldClose(window))
     {
