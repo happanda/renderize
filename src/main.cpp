81ae7ffcd38fc75878ac8ec475ae9e1eaf885c98
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-09-27 18:23:24 +0300

    loading and drawing textures

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <GLFW/glfw3.h>
 
 #include "shaders/program.h"
+#include "SOIL.h"
 
 
 size_t const sWinWidth = 800;
@@ -17,24 +18,31 @@ GLchar const* sVertexShader = R"(#version 330 core
 
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
+layout(location = 2) in vec2 texCoords;
 out vec3 vertColor;
+out vec2 texCoord;
 uniform vec3 shiftPos;
 
 void main()
 {
     gl_Position = vec4(position + shiftPos, 1.0f);
-    vertColor = color;//vec3(0.0f, 0.0f, 0.0f);
+    vertColor = color;
+    texCoord = vec2(texCoords.x, 1.0f - texCoords.y);
 })";
 
 GLchar const* sFragmentShaderOrange = R"(#version 330 core
 
 in vec3 vertColor;
+in vec2 texCoord;
 out vec4 color;
+
 uniform vec4 uniColor;
+uniform sampler2D texData1;
+uniform sampler2D texData2;
 
 void main()
 {
-    color = vec4(vertColor, 1.0f);
+    color = mix(texture(texData1, texCoord), texture(texData2, texCoord), 0.2);// * vec4(vertColor, 1.0f);
 }
 )";
 
@@ -142,24 +150,74 @@ int main(void)
         }
     }
 
-    // Geometry to draw
-    float const PI = 4.0f * atan(1.0f);
-    int const numVerts = 500;
-    std::vector<GLfloat> vertices;
+        // Texture loading
+    GLuint textures[2];
+    glGenTextures(2, textures);
 
-    
-    std::vector<GLint> indices;
-    for (int i = 0; i < numVerts + 2; ++i)
-    {
-        indices.push_back(i);
-    }
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    int texWidth{ 0 }, texHeight{ 0 };
+    auto image = SOIL_load_image("../../tex/crate.jpg", &texWidth, &texHeight, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    image = SOIL_load_image("../../tex/awesomeface.png", &texWidth, &texHeight, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+        // Geometry to draw
+    GLfloat vertices[] = {
+        // Positions        // Colors         // Texture Coords
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // Top Right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // Bottom Right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Left
+        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // Top Left 
+    };
+    GLuint indices[] = {
+        0, 1, 3,   // First Triangle
+        1, 2, 3    // Second Triangle
+    };
+
+    GLfloat texCoords[] = {
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        0.5f, 1.0f
+    };
 
 
     GLuint VBO;
     glGenBuffers(1, &VBO);
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), static_cast<GLvoid*>(0));
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(2);
+    }
     
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
         // Game Loop
     while (!glfwWindowShouldClose(window))
@@ -177,53 +235,20 @@ int main(void)
         GLfloat blueColor = val;
         GLint uniColorLocation = glGetUniformLocation(shaderProgOr, "uniColor");
 
-        //GLint shiftPosLocation = glGetUniformLocation(shaderProgOr, "shiftPos");
-        //glUniform3f(shiftPosLocation, std::sin(curTime), 0.0f, 0.0f);
-        
         shaderProgOr.use();
         glUniform4f(uniColorLocation, redColor, greenColor, blueColor, 1.0f);
         
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glUniform1i(glGetUniformLocation(shaderProgOr, "texData1"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
+        glUniform1i(glGetUniformLocation(shaderProgOr, "texData2"), 1);
+
         glBindVertexArray(VAO);
-        {
-            vertices.clear();
-            vertices.push_back(0.0f);
-            vertices.push_back(0.0f);
-            vertices.push_back(0.0f);
-            vertices.push_back(1.0f);
-            vertices.push_back(1.0f);
-            vertices.push_back(1.0f);
-            for (int i = 0; i < numVerts; ++i)
-            {
-                float const alpha = 2.0f * PI * i / numVerts;
-                GLfloat xPos = std::cos(alpha) * (0.5f + 0.000015f * std::sin(2 * curTime) + 0.05f * std::sin(3 * (alpha + curTime / 2.0f)));
-                GLfloat yPos = std::sin(alpha) * (0.5f + 0.000015f * std::sin(2 * curTime) + 0.05f * std::sin(3 * (alpha + curTime / 2.0f)));
-                vertices.push_back(xPos);
-                vertices.push_back(yPos);
-                vertices.push_back(0.0f);
-                vertices.push_back(xPos);
-                vertices.push_back(yPos);
-                vertices.push_back(xPos * yPos);
-            }
-            vertices.push_back(vertices[6]);
-            vertices.push_back(vertices[7]);
-            vertices.push_back(vertices[8]);
-            vertices.push_back(vertices[6]);
-            vertices.push_back(vertices[7]);
-            vertices.push_back(vertices[8]);
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_DYNAMIC_DRAW);
-
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), static_cast<GLvoid*>(0));
-            glEnableVertexAttribArray(0);
-
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-            glEnableVertexAttribArray(1);
-        }
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 2 * (numVerts + 2));
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        //glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         glfwSwapBuffers(window);
     }
