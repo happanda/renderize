8fd981236ce978d0989dfe0414a14bcca70bb6c9
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-10-06 19:10:42 +0300

    removed unnecessary data; fixed conversion warnings

--- src/main.cpp
+++ src/main.cpp
@@ -194,10 +194,6 @@ int main(void)
         -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
     };
-    GLuint indices[] = {
-        0, 1, 3,   // First Triangle
-        1, 2, 3    // Second Triangle
-    };
 
 
     GLuint VBO;
@@ -210,9 +206,6 @@ int main(void)
     {
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), static_cast<GLvoid*>(0));
         glEnableVertexAttribArray(0);
@@ -334,12 +327,15 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int modi
 
 void mouseCallback(GLFWwindow* window, double x, double y)
 {
-    static GLfloat lastX = x, lastY = y;
-    GLfloat xDiff = x - lastX;
-    GLfloat yDiff = y - lastY;
+    float const xFloat = static_cast<GLfloat>(x);
+    float const yFloat = static_cast<GLfloat>(y);
 
-    lastX = x;
-    lastY = y;
+    static GLfloat lastX = xFloat, lastY = yFloat;
+    GLfloat xDiff = xFloat - lastX;
+    GLfloat yDiff = yFloat - lastY;
+
+    lastX = xFloat;
+    lastY = yFloat;
 
     GLfloat sensitivity = 0.05f;
     xDiff *= sensitivity;
@@ -351,7 +347,7 @@ void mouseCallback(GLFWwindow* window, double x, double y)
 
 void scrollCallback(GLFWwindow* window, double xDiff, double yDiff)
 {
-    sCamera.fov(sCamera.fov() - yDiff);
+    sCamera.fov(sCamera.fov() - static_cast<float>(yDiff));
 }
 
 void moveCamera(float dt)
--- src/shaders/vert_simple.c
+++ src/shaders/vert_simple.c
@@ -3,7 +3,6 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 texCoords;
 
-out vec3 vertColor;
 out vec2 texCoord;
 
 uniform mat4 model;
@@ -13,6 +12,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(position, 1.0f);
-    vertColor = vec3(0.0f, 0.0f, 0.0f);
     texCoord = vec2(texCoords.x, 1.0f - texCoords.y);
 }
