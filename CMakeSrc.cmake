bf6dc27396f6537dd1b420d1cc3ef658f88a11be
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-10-27 00:47:26 +0300

    moved shader sources to separate folder

--- CMakeSrc.cmake
+++ CMakeSrc.cmake
@@ -19,11 +19,11 @@ ${SourcePath}/textures/texture.cpp
 )
 
 set(SOURCE_FILES_SHADERS_SOURCE
-${SourcePath}/shaders/cube.vert
-${SourcePath}/shaders/cube.frag
-${SourcePath}/shaders/lamp.frag
-${SourcePath}/shaders/simple.vert
-${SourcePath}/shaders/visual.frag
+${SourcePath}/shaders_src/cube.vert
+${SourcePath}/shaders_src/cube.frag
+${SourcePath}/shaders_src/lamp.frag
+${SourcePath}/shaders_src/simple.vert
+${SourcePath}/shaders_src/visual.frag
 )
 
 set(SOURCE_FILES
